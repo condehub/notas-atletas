2e6bcdefb41acf59fdf95849fc3ec348889fbafc
jaovbcx <v.conde@academico.ifrr.edu.br>
2026-03-17 22:51:08 -0400

    dados-atletas file added

---
+++ dados-atletas.js
@@ -0,0 +1,13 @@
+class Atleta {
+  construct(nome, idade, peso, altura, notas) {
+    this.nome = nome;
+    this.idade = idade;
+    this.peso = peso;
+    this.altura = altura;
+    this.notas = notas;
+  }
+}
+
+const atleta = new Atleta("Cesar Abascal",
+    30, 80, 1.70,
+    [10, 9.34, 8.42, 10, 7.88]);
